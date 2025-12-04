f4c3f5f92a51081d73f58023bb9be44dbd69ce31
dipanshuk035 <dipanshuk035@gmail.com>
2025-12-04 13:50:10 +0530

    movie card added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+const movie = {
+  imdbID: "tt1375666",
+  Title: "The Avengers",
+  Year: "2012",
+  Poster:
+    "https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg",
+  Rating: "7.8",
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <MovieCard />
     </div>
   );
 }
 
 export default App;
+
+function MovieCard() {
+  return (
+    <div className="movie-card">
+      <img src={movie.Poster} alt={`${movie.Title} poster`} />
+      <div>
+        <h3>{movie.Title}</h3>
+        <p>
+          <span>🗓</span>
+          <span>{movie.Year}</span>
+        </p>
+        <p>
+          <span>⭐</span>
+          {movie.Rating}
+        </p>
+      </div>
+    </div>
+  );
+}
