cf464043e01856a7f9bb769949dc8fe6b46f305a
dipanshuk035 <dipanshuk035@gmail.com>
2025-12-11 17:49:37 +0530

    fetch function added

--- src/App.js
+++ src/App.js
@@ -1,27 +1,59 @@
 import logo from "./logo.svg";
 import "./App.css";
+import { useEffect, useState } from "react";
 
-const movie = {
-  imdbID: "tt1375666",
-  Title: "The Avengers",
-  Year: "2012",
-  Poster:
-    "https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg",
-  Rating: "7.8",
-};
+// const movie = {
+//   imdbID: "tt1375666",
+//   Title: "The Avengers",
+//   Year: "2012",
+//   Poster:
+//     "https://m.media-amazon.com/images/M/MV5BNGE0YTVjNzUtNzJjOS00NGNlLTgxMzctZTY4YTE1Y2Y1ZTU4XkEyXkFqcGc@._V1_SX300.jpg",
+//   Rating: "7.8",
+// };
 
-export default function App() {
+function App() {
+  const [searchedMovies, setSearchedMovies] = useState([]);
+  const [query, setQuery] = useState("avengers");
+
+  useEffect(
+    function () {
+      async function fetchMovies() {
+        try {
+          const res = await fetch(
+            `http://www.omdbapi.com/?i=tt3896198&apikey=f28062cf&s=${query}`
+          );
+          const data = await res.json();
+          setSearchedMovies((data) => data);
+        } catch (err) {
+          console.log(err);
+        }
+      }
+      fetchMovies();
+    },
+    [query]
+  );
   return (
     <div className="App">
-      <MovieCard />
-      <TimerComponent />
+      <MovieList searchedMovies={searchedMovies} />
     </div>
   );
 }
 
-function MovieCard() {
+export default App;
+
+function MovieList({ searchedMovies }) {
   return (
-    <div className="movie-card">
+    <ul>
+      {searchedMovies.map((movie) => (
+        <MovieCard movie={movie} key={movie.imdbID} />
+      ))}
+    </ul>
+  );
+}
+
+function MovieCard({ movie }) {
+  return (
+    <li className="movie-card">
       <img src={movie.Poster} alt={`${movie.Title} poster`} />
       <div>
         <h3>{movie.Title}</h3>
@@ -34,72 +66,6 @@ function MovieCard() {
           {movie.Rating}
         </p>
       </div>
-    </div>
+    </li>
   );
 }
-
-const TimerComponent = () => {
-  const [seconds, setSeconds] = useState(0);
-
-  const [isRunning, setIsRunning] = useState(false);
-
-  const intervalRef = useRef(null);
-
-  useEffect(() => {
-    if (isRunning) {
-      intervalRef.current = setInterval(() => {
-        setSeconds((prevSeconds) => prevSeconds + 1);
-      }, 1000);
-    } else {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
-      }
-    }
-
-    return () => {
-      if (intervalRef.current) {
-        clearInterval(intervalRef.current);
-      }
-    };
-  }, [isRunning]);
-
-  const handleStart = () => {
-    if (!isRunning) {
-      setIsRunning(true);
-    }
-  };
-
-  const handlePause = () => {
-    setIsRunning(false);
-  };
-
-  const handleReset = () => {
-    setIsRunning(false);
-    setSeconds(0);
-  };
-
-  const btnStyle = {
-    cursor: "pointer",
-    border: "none",
-    height: "40px",
-    width: "100px",
-  };
-
-  return (
-    <div style={{ padding: "20px", fontFamily: "Arial, sans-serif" }}>
-      <h3>Timer</h3>
-      <p style={{ fontSize: "20px", fontWeight: "bold" }}>{seconds} seconds</p>
-      <div style={{ display: "flex", gap: "10px" }}>
-        <button style={btnStyle} onClick={handleStart} disabled={isRunning}>
-          Start
-        </button>
-        <button style={btnStyle} onClick={handlePause} disabled={!isRunning}>
-          Pause
-        </button>
-        <button style={btnStyle} onClick={handleReset}>
-          Reset
-        </button>
-      </div>
-    </div>
-  );
-};
